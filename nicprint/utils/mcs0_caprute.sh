65440c17334d0bcb3976e935aad8ac41d6b4ebeb
carguelloM <cnarguellomartinez@gmail.com>
2026-09-23 15:32:28 -0400

    fixing typo in ack receptor script

--- nicprint/utils/mcs0_caprute.sh
+++ nicprint/utils/mcs0_caprute.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 ### Script that sends frames at MCS0 and captures ACKS
-sudo tcpdump -i sdr0 -e -s 0 -n -w "acks_mcs_${i}.pcap" 'radio[radio[2]] = 0xd4 and radio[radio[2]+1] = 0x00 and radio[radio[2]+4] = 0x22 and radio[radio[2]+5] = 0x23 and radio[radio[2]+6] = 0x23 and radio[radio[2]+7] = 0x23 and radio[radio[2]+8] = 0x23' > /dev/null 2>&1 </dev/null &
+sudo tcpdump -i sdr0 -e -s 0 -n -w "acks_mcs_0.pcap" 'radio[radio[2]] = 0xd4 and radio[radio[2]+1] = 0x00 and radio[radio[2]+4] = 0x22 and radio[radio[2]+5] = 0x23 and radio[radio[2]+6] = 0x23 and radio[radio[2]+7] = 0x23 and radio[radio[2]+8] = 0x23' > /dev/null 2>&1 </dev/null &
     sleep 0.2
     
 ### send frames
